da0124ea9b235a03fbe2a89b0246a458ce45158b
Michał Stankiewicz <kontakt@stankiewiczm.eu>
2024-10-28 14:31:13 +0100

    24105
    - Changed logo
    - Changed description
    - Added special themes
    - Changed badge
    - Changed OpenGraph image

---
+++ event-themes.js
@@ -0,0 +1,39 @@
+var date = new Date();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+
+console.log(month, day);
+
+function addPride() {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'events/pride.css';
+    link.classList.add('specialTheme');
+    document.head.appendChild(link);
+}
+
+function addHalloween() {
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'events/halloween.css';
+    link.classList.add('specialTheme');
+    document.head.appendChild(link);
+}
+
+function checkDate() {
+    if (month == 6 && day >= 1 && day <= 30) {
+        addPride();
+    }
+    if (month == 10 && day >= 1 && day <= 31) {
+        addHalloween();
+    }
+}
+
+function disableTheme() {
+    var link = document.querySelector('.specialTheme');
+    if (link) {
+        link.remove();
+    }
+}
+
+checkDate();
